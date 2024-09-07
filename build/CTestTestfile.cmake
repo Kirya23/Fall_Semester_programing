446e237dc4e675bc7523fd8000a510f5381230a7
Kirya <Basketman2019@mail.ru>
2024-09-07 10:17:28 +0700

    Cmake add

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester-programing--1/lab1_actions
+# Build directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester-programing--1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester-programing--1/lab1_actions/CMakeLists.txt;74;add_test;C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester-programing--1/lab1_actions/CMakeLists.txt;0;")
