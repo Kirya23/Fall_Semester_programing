d995535c86e7611bb2e74c802e85bacb27c6c2e3
Kirya <Basketman2019@mail.ru>
2024-10-01 16:20:52 +0700

    first commit in 2 branch

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-#include "lib.h"
-
 #include <iostream>
 
-int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main(int, char**){
+    std::cout << "Hello, from lab2!\n";
 }
