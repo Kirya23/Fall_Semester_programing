867b1f1d3961d46e97f973436bc315d89f7488bf
Kirya <Basketman2019@mail.ru>
2024-10-12 12:52:48 +0700

    try_321

--- test_ip_address.cpp
+++ test_ip_address.cpp
@@ -1,39 +1,69 @@
-#include <gtest/gtest.h>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include "main.cpp"
+#include "gtest/gtest.h"
+#include "main.cpp" // Включите файл с кодом, который вы хотите протестировать
 
-using namespace std;
-
-
-TEST(IPAddressTest, ValidIPAddress) {
-    IPAddress ip("192.168.0.1");
-    EXPECT_EQ(ip.n1, 192);
-    EXPECT_EQ(ip.n2, 168);
-    EXPECT_EQ(ip.n3, 0);
-    EXPECT_EQ(ip.n4, 1);
-}
-
-TEST(IPAddressTest, ToString) {
-    IPAddress ip("10.0.0.5");
-    EXPECT_EQ(ip.toString(), "10.0.0.5");
-}
-
-TEST(SortIPAddressesTest, SortsCorrectly) {
-    std::vector<IPAddress> ips = {
+// Тестовый набор IP-адресов для проверки
+vector<IPAddress> createTestIPAddresses() {
+    vector<IPAddress> testAddresses = {
         IPAddress("192.168.1.1"),
         IPAddress("10.0.0.1"),
-        IPAddress("172.16.0.5")
+        IPAddress("172.16.0.1"),
+        IPAddress("1.2.3.4"),
+        IPAddress("46.70.1.2"),
+        IPAddress("46.1.2.3"),
+        IPAddress("192.168.46.1"),
+        IPAddress("1.2.3.46")
     };
+    return testAddresses;
+}
 
-    sort(ips.begin(), ips.end(), compareIP);
+TEST(IPFilteringTest, SortInReverseLexicographicalOrder) {
+    vector<IPAddress> ipAddresses = createTestIPAddresses();
+    sort(ipAddresses.begin(), ipAddresses.end(), compareIP);
 
-    EXPECT_EQ(ips[0].toString(), "10.0.0.1");
-    EXPECT_EQ(ips[1].toString(), "172.16.0.5");
-    EXPECT_EQ(ips[2].toString(), "192.168.1.1");
+    // Проверка, что адреса отсортированы в обратном лексикографическом порядке
+    ASSERT_EQ(ipAddresses[0].toString(), "192.168.46.1");
+    ASSERT_EQ(ipAddresses[1].toString(), "192.168.1.1");
+    ASSERT_EQ(ipAddresses[2].toString(), "172.16.0.1");
+    ASSERT_EQ(ipAddresses[3].toString(), "46.70.1.2");
+    ASSERT_EQ(ipAddresses[4].toString(), "46.1.2.3");
+    ASSERT_EQ(ipAddresses[5].toString(), "10.0.0.1");
+    ASSERT_EQ(ipAddresses[6].toString(), "1.2.3.46");
+    ASSERT_EQ(ipAddresses[7].toString(), "1.2.3.4");
+}
+
+TEST(IPFilteringTest, FilterFirstByte) {
+    vector<IPAddress> ipAddresses = createTestIPAddresses();
+    vector<string> filteredAddresses;
+
+    for (const auto& ip : ipAddresses) {
+        if (ip.n1 == 1) {
+            filteredAddresses.push_back(ip.toString());
+        }
+    }
+
+    // Проверка фильтрации по первому байту
+    ASSERT_EQ(filteredAddresses.size(), 3);
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "192.168.1.1"), filteredAddresses.end());
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "1.2.3.4"), filteredAddresses.end());
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "1.2.3.46"), filteredAddresses.end());
+}
+
+TEST(IPFilteringTest, FilterAnyByte) {
+    vector<IPAddress> ipAddresses = createTestIPAddresses();
+    vector<string> filteredAddresses;
+
+    for (const auto& ip : ipAddresses) {
+        if (ip.n1 == 46 || ip.n2 == 46 || ip.n3 == 46 || ip.n4 == 46) {
+            filteredAddresses.push_back(ip.toString());
+        }
+    }
+
+    // Проверка фильтрации по любому байту
+    ASSERT_EQ(filteredAddresses.size(), 4);
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "46.70.1.2"), filteredAddresses.end());
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "46.1.2.3"), filteredAddresses.end());
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "192.168.46.1"), filteredAddresses.end());
+    ASSERT_NE(find(filteredAddresses.begin(), filteredAddresses.end(), "1.2.3.46"), filteredAddresses.end());
 }
 
 int main(int argc, char **argv) {
