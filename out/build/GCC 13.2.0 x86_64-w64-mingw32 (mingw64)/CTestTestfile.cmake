0e63daaabb138cbc706716fed18a6ee24b75ccd0
Kirya <Basketman2019@mail.ru>
2024-10-16 21:05:09 +0700

    try

--- out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing
-# Build directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing/out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_lab2 "test_ip_address")
-set_tests_properties(test_lab2 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing/CMakeLists.txt;89;add_test;C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing/CMakeLists.txt;0;")
