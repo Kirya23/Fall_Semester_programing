d08fb683515cff45a77ab032cc217d4334d09623
Kirya <Basketman2019@mail.ru>
2024-11-17 13:40:52 +0700

    first commit

--- out/build/GCC 14.1.0 x86_64-w64-mingw32 (ucrt64)/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing
-# Build directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing/out/build/GCC 14.1.0 x86_64-w64-mingw32 (ucrt64)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
