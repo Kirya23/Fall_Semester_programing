b9fb505bb3f1c3db44fcec4df64cae708227a91b
Kirya <Basketman2019@mail.ru>
2024-11-17 13:48:25 +0700

    try 3

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing
+# Build directory: C:/Users/PiroZHoCheck_2.0/Desktop/Fall_Semester_programing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
