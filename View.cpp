05c301780a96a0f9e2c2f593b63268af522902a4
Kirya <Basketman2019@mail.ru>
2024-12-09 20:50:15 +0700

    push lab7

--- View.cpp
+++
@@ -1,8 +0,0 @@
-#include "view.h"
-#include <iostream>
-
-void View::displayPrimitives(const std::vector<std::shared_ptr<Primitive>>& primitives) {
-    for (const auto& primitive : primitives) {
-        std::cout << "Primitive: " << primitive->getType() << std::endl;
-    }
-}
--- main.cpp
+++ main.cpp
@@ -1,30 +1,5 @@
 #include <iostream>
-#include <memory>
-#include "controller.h"
 
-int main() {
-    Controller controller;
-
-    // Создание нового документа
-    controller.createDocument("First document");
-
-    //Импорт документа из файла
-    controller.importDocument("top_секрет.txt");
-
-    // Экспорт документа в файл
-    controller.exportDocument("top_секрет.txt");
-
-    // Создание графических примитивов
-    controller.createPrimitive("Мой ребёнок","Circle", 10, 20, 5); // Пример: Circle с координатами (10, 20) и радиусом 5
-    controller.createPrimitive("Мой второй ребёнок","Rectangle", 14, 2, 10);
-    controller.createPrimitive("Мой третий ребёнок","Circle", 10, 20, 5); // Пример: Circle с координатами (10, 20) и радиусом 5
-
-    std::cout<<"Вывод примитивов:"<< std::endl;
-    controller.Viewprimitives();
-
-    std::cout<<"После удаления:"<< std::endl;
-    // Удаление графического примитива
-    controller.deletePrimitive(0); // Удаляем первый примитив
-    controller.Viewprimitives();
-    return 0;
+int main(int, char**){
+    std::cout << "Hello, from lab7!\n";
 }
